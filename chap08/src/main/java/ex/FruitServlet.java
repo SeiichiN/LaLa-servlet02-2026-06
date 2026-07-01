f2da3174897903c7f87e9ab5b11cc6db482dffff
SeiichiN <billie175@gmail.com>
2026-07-01 15:11:06 +0900

    chap09 7/1

---
+++ chap08/src/main/java/ex/FruitServlet.java
@@ -0,0 +1,26 @@
+package ex;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Fruit fruit = new Fruit("いちご", 700);
+		HttpSession session = request.getSession();
+		session.setAttribute("fruit", fruit);
+		RequestDispatcher d =
+				request.getRequestDispatcher("WEB-INF/ex/fruit.jsp");
+		d.forward(request, response);
+	}
+
+}
---
+++ chap09/src/main/java/ex/FruitServlet.java
@@ -0,0 +1,26 @@
+package ex;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Fruit fruit = new Fruit("ぶどう", 700);
+		ServletContext application = this.getServletContext();
+		application.setAttribute("fruit", fruit);
+		RequestDispatcher d =
+				request.getRequestDispatcher("WEB-INF/ex/fruit.jsp");
+		d.forward(request, response);
+	}
+
+}
